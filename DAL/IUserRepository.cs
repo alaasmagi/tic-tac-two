f7faa97179e9cce8db61ddfe50ae065d5912fa42
Aleksander Laasmägi <aleksander.laasmagi@gmail.com>
2024-12-11 21:21:20 +0200

    Updated solution

--- DAL/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-using Domain;
-
-namespace DAL;
-
-public interface IUserRepository
-{
-    public void CreateUser(UserEntity newUser);
-    public UserEntity LogIn(string username, string passHash);
-}
